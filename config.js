19bfc4bf29c0fb35777e1ab2d9402a4021a057a9
Julien Enselme <julien.enselme@centrale-marseille.fr>
2017-08-19 17:05:18 +0200

    Add  build scripts

---
+++ config.js
@@ -0,0 +1,10 @@
+System.config({
+  defaultJSExtensions: true,
+  transpiler: "none",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {}
+});
